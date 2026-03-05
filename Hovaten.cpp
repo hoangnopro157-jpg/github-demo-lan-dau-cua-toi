b2d9d77bb44a6400d68c048826fc793fdfa77477
unknown <hoangnopro157@gmail.com>
2026-03-05 07:12:38 +0700

    Da add them Hovaten.cpp

---
+++ Hovaten.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void  In_ThongtinSV(char Hoten, int mssv){
+    cout<<"Dinh Bat Hoang"<<endl;
+    cout<<"3125560017";
+}
+int main(){
+    char Hoten;
+    int mssv;
+    In_ThongtinSV(Hoten,mssv);
+    return 0;
+}
